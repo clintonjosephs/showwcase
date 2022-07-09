ea1f74f42a319de2115018b6b1da8bdf93b63a2f
clintonjosephs <clintonjosephs26@gmail.com>
2022-07-09 14:01:22 +0100

    Abstracted footer as an independent component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
 import { AppProps } from 'next/app';
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className="container">
+        <Component {...pageProps} />
+      </div>
+      <footer className="footer">
+        <a
+          href="https://showwcase.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by Showwcase
+        </a>
+      </footer>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,35 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
-const Home = () => (
-  <div className={styles.container}>
-    <Head>
-      <title>Showwcase</title>
-      <meta
-        name="description"
-        content="Welcome to showcase app, where you can network and showcase your skills and achievements as a software developer!!"
-      />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Showwcase</title>
+        <meta
+          name="description"
+          content="Welcome to showcase app, where you can network and showcase your skills and achievements as a software developer!!"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-    <main className={styles.main}>
-      <h6 className={styles.title}>
-        Hi there! Welcome to your education showcase.
-      </h6>
+      <main className={styles.main}>
+        <h6 className={styles.title}>
+          Hi there! Welcome to your education showcase.
+        </h6>
 
-      <div>
-        <form className={styles.form}>
-          <label htmlFor="name">
-            Type your name and click "Enter" below to begin!
-          </label>
-          <input type="text" placeholder="Your name" id="name" />
-          <button type="submit">Enter</button>
-        </form>
-      </div>
-    </main>
-
-    <footer className={styles.footer}>
-      <a href="https://showwcase.com" target="_blank" rel="noopener noreferrer">
-        Powered by Showwcase
-      </a>
-    </footer>
-  </div>
-);
+        <div>
+          <form className={styles.form}>
+            <label htmlFor="name">
+              Type your name and click "Enter" below to begin!
+            </label>
+            <input type="text" placeholder="Your name" id="name" required maxLength={200} />
+            <button type="submit">Enter</button>
+          </form>
+        </div>
+      </main>
+    </>
+  );
+};
 
 export default Home;
